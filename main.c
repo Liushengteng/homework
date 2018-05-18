573456de2e164d329ddcf24ffa570a94a0d68e2c
劉勝騰 <zxc13148120@gmail.com>
2018-05-18 14:06:58 +0800

    Fifth commit

--- main.c
+++ main.c
@@ -4,32 +4,33 @@
 void show(int a[]),bubble(int a[]);
 int main(int argc, char *argv[])
 {
-int FirstNumber,SecondNumber,ThirdNumber,ForthNumber,FifthNumber;
+int FirstNumber,SecondNumber,ThirdNumber,ForthNumber,FifthNumber;   //設定變數 
 printf("請輸入:");
-scanf("%d %d %d %d %d",&FirstNumber,&SecondNumber,&ThirdNumber,&ForthNumber,&FifthNumber);
-int data[SIZE]={FirstNumber,SecondNumber,ThirdNumber,ForthNumber,FifthNumber};
+scanf("%d %d %d %d %d",&FirstNumber,&SecondNumber,&ThirdNumber,&ForthNumber,&FifthNumber);   //輸入變數數字 
+int data[SIZE]={FirstNumber,SecondNumber,ThirdNumber,ForthNumber,FifthNumber}; //陣列排序 
 printf("排序前..\n");
-show(data);
-bubble(data);
+show(data);       //呼叫函數 
+bubble(data);     //氣泡排序 
 printf("排序後..\n");
-show(data); 
+show(data);       //呼叫函數
 system("PAUSE"); 
 return 0;
 }
-void show(int a[]) 
+void show(int a[])   //顯示函數 
 { 
-int i;
-for(i=0;i<SIZE;i++)
-printf(" %d",a[i]);
-printf("\n");
+	int i;
+	for(i=0;i<SIZE;i++)
+	printf(" %d",a[i]);
+	printf("\n");
 }
-void bubble(int a[])
+void bubble(int a[])  //排序函數 
 {
-int i,j,temp;
-for(i=1;i<SIZE;i++)
-for(j=0;j<(SIZE-i);j++)
-if(a[j]<a[j+1])
-{temp=a[j];
-a[j]=a[j+1];
-a[j+1]=temp;}
+	int i,j,temp;
+		for(i=1;i<SIZE;i++)
+		for(j=0;j<(SIZE-i);j++)
+		if(a[j]<a[j+1])
+{	
+	temp=a[j];
+	a[j]=a[j+1];
+	a[j+1]=temp;}
 }
